dcfef3889991b829650e3fd75f28032aee5e5838
David <dmaine6711@gmail.com>
2024-01-10 09:57:14 -0500

    finish front end auth

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,13 +4,14 @@ import App from './App';
 import './index.css';
 import configureStore from './store/store';
 import { Provider } from 'react-redux';
-
+import csrfFetch from './store/csrf';
 
 
 const store = configureStore();
 
 if (import.meta.env.MODE !== "production") {
   window.store = store;
+  window.csrfFetch = csrfFetch;
 }
 
 
--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -1,20 +1,22 @@
-import {createStore, applyMiddleware, compose, combineReducers} from 'redux;'
-import thunk from 'redux-thunk';
+import {createStore, applyMiddleware, compose, combineReducers} from 'redux';
+import {thunk} from 'redux-thunk';
 
 
 const rootReducer = combineReducers({
 
-})
+});
 
 let enhancer;
 if (import.meta.env.MODE === 'production') {
-    enhancer = applyMiddleware(thunk);
+  enhancer = applyMiddleware(thunk);
 } else {
-    const logger = await import("redux-logger").default;
-    const composeEnhancers = 
-        window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+  const logger = (await import("redux-logger")).default;
+  const composeEnhancers =
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
   enhancer = composeEnhancers(applyMiddleware(thunk, logger));
 }
+
+
 
 function configureStore(preloadedState) {
     return createStore(rootReducer, preloadedState, enhancer)
